bc22e7a0a2a6bfbd5e1dc7f2b95b15e9cadd02d3
seppsher <lukasz.kowalskii@interia.pl>
2025-03-08 14:09:47 +0100

    Loader implementation

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LoadingService } from '../shared/services/loading.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatProgressSpinnerModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app';
+  constructor(public loadingService: LoadingService) {}
 }
